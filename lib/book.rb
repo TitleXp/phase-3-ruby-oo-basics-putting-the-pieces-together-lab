e3b8af3725a06fec8d9dbfa7772b0ff5d870ffbd
Surapat Mekvanich <titleyugio@gmail.com>
2023-01-10 10:31:36 -0800

    commit tree 79b0e9b8b5d8fccc0c533c394c6903642517ebdc

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,34 @@
+require "pry"
+class Book #class is a blueprint/template
+
+    def initialize(title) #object is an instance of class
+        @title = title
+    end
 
 
+end
+
+new_book = Book.new()
+# binding.pry
+
+
+
+
+
+
+
+
+
+
+
+# #Matt Eva example
+# class Apartment
+#     #each apartment has number of rooms, number of bathrooms
+#     #gas or electric stove
+
+# end
+
+
+# # studio apartment ( this is our instance ) - one room, electric stove, one bathroom.
+# # studio apartment - one room, gas stove, one bathroom.
+# # 3 bedroom apartment - 3 rooms, 2 bathrooms, electric stove
